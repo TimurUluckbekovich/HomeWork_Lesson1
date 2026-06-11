7e23c54644e20ae7a32707355bbb4e92753e144b
TimurUluckbekovich <hakimovt09@gmail.com>
2026-06-11 17:14:34 +0600

    Добавил второе домашнее задание

--- lesson_2/home_work.py
+++ lesson_2/home_work.py
@@ -1,2 +1,61 @@
 """  Task 1:  """
-a = 22
+class Notification:
+    def __init__(self, user, message):
+        self.user = user
+        self.message = message
+    def send(self):
+        pass
+
+class  EmailNotification(Notification):
+    def send(self):
+        print(f"Email: For {self.user}\n     {self.message}\n")
+
+class SMSNotification(Notification):
+    def send(self):
+        print(f"SMS: For {self.user}\n     {self.message}\n")
+
+class TelegramNotification(Notification):
+    def send(self):
+        print(f"Telegram: For {self.user}\n     {self.message}\n")
+
+def notify_all(notifications):
+    for notification in notifications:
+        notification.send()
+
+
+email = EmailNotification("Jack", "Good Morning Jack!")
+sms = SMSNotification("Bob", "How are you?")
+tg = TelegramNotification("Ali", "How old are you?")
+
+arr = [email, sms, tg]
+
+notify_all(arr)
+
+"""  Task 2:  """
+class Transport:
+    def __init__(self, name, speed):
+        self.name = name
+        self.speed = speed
+    def move(self):
+        pass
+
+class Car(Transport):
+    def move(self):
+        print(f"Car: {self.name}\nSpeed: {self.speed}\n")
+
+class Plane(Transport):
+    def move(self):
+        print(f"Plane: {self.name}\nSpeed: {self.speed}\n")
+
+class Boat(Transport):
+    def move(self):
+        print(f"Boat: {self.name}\nSpeed: {self.speed}\n")
+
+def start_trip(transports):
+    for transport in transports:
+        transport.move()
+
+transports = [Car("BMW",120), Plane("Boeing", 900), Boat("Yacht", 60)]
+
+start_trip(transports)
+
--- lesson_2/main.py
+++ lesson_2/main.py
@@ -25,32 +25,92 @@
 # d.breath()
 # d.gav()
 
-class User:
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
+# class User:
+#     def __init__(self, name, email):
+#         self.name = name
+#         self.email = email
+#
+#     def get_info(self):
+#         return f"Name {self.name}, Email: {self.email}"
+#
+# class Teacher(User):
+#     def __init__(self, name, email, subject):
+#         super().__init__(name, email)
+#         self.subject = subject
+#
+#     def teach(self):
+#         return f"{self.name} teaches {self.subject}"
+#
+# class Admin(User):
+#     def ban_user(self, user_name):
+#         return f"{self.name} banned {user_name.name}"
+#
+# t = Teacher("Alice", "alice@gmail.com", "history")
+# b = Admin("Bob", "bob@gmail.com")
+#
+# print(t.get_info())
+# print(t.teach())
+# print(b.get_info())
+# print(b.ban_user(t))
 
-    def get_info(self):
-        return f"Name {self.name}, Email: {self.email}"
 
-class Teacher(User):
-    def __init__(self, name, email, subject):
-        super().__init__(name, email)
-        self.subject = subject
+"""  Полиморфизм  """
+# class PaymentMethod():
+#     def pay(self, payment):
+#         pass
+#
+# class CardPayment(PaymentMethod):
+#     def pay(self, payment):
+#         print(f"Оплата {payment} руб. картой")
+#
+# class CashPayment(PaymentMethod):
+#     def pay(self, payment):
+#         print(f"Оплата {payment} руб. наличными")
+#
+# class CryptPayment(PaymentMethod):
+#     def pay(self, payment):
+#         print(f"Оплата {payment} руб. криптовалютой")
+#
+# def process_payment(payment_method, payment):
+#     payment_method.pay(payment)
+#
+# card = CardPayment()
+# cash = CashPayment()
+# crypt = CryptPayment()
+#
+# process_payment(card, 1000)
+# process_payment(cash, 500)
+# process_payment(crypt, 2000)
 
-    def teach(self):
-        return f"{self.name} teaches {self.subject}"
+""" Task """
+class Notification:
+    def __init__(self, user, message):
+        self.user = user
+        self.message = message
+    def send(self):
+        pass
 
-class Admin(User):
-    def ban_user(self, user_name):
-        return f"{self.name} banned {user_name.name}"
+class  EmailNotification(Notification):
+    def send(self):
+        print(f"Email: For {self.user}\n     {self.message}")
 
-t = Teacher("Alice", "alice@gmail.com", "history")
-b = Admin("Bob", "bob@gmail.com")
+class SMSNotification(Notification):
+    def send(self):
+        print(f"SMS: For {self.user}\n     {self.message}")
 
-print(t.get_info())
-print(t.teach())
-print(b.get_info())
-print(b.ban_user(t))
+class TelegramNotification(Notification):
+    def send(self):
+        print(f"Telegram: For {self.user}\n     {self.message}")
+
+def notify_all(notifications):
+    for notification in notifications:
+        notification.send()
 
 
+email = EmailNotification("Jack", "Good Morning Jack!")
+sms = SMSNotification("Bob", "How are you?")
+tg = TelegramNotification("Ali", "How old are you?")
+
+arr = [email, sms, tg]
+
+notify_all(arr)
